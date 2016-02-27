3ecd0b5920cb90fb6f754bdfc85c4bd407a69bda
Harry Ng <harryworld@gmail.com>
2016-02-27 22:02:40 +0800

    Set the basic structure of NSCollectionView using default flow layout

---
+++ NSCollectionViewDragDrop/MyItem.swift
@@ -0,0 +1,18 @@
+//
+//  MyItem.swift
+//  NSCollectionViewDragDrop
+//
+//  Created by Harry Ng on 27/2/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class MyItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- NSCollectionViewDragDrop/ViewController.swift
+++ NSCollectionViewDragDrop/ViewController.swift
@@ -10,6 +10,10 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var strings = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,3 +29,22 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSCollectionViewDataSource {
+    
+    func collectionView(collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return strings.count
+    }
+    
+    func collectionView(collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
+        return collectionView.makeItemWithIdentifier("MyItem", forIndexPath: indexPath)
+    }
+    
+}
+
+extension ViewController: NSCollectionViewDelegate {
+    
+    func collectionView(collectionView: NSCollectionView, willDisplayItem item: NSCollectionViewItem, forRepresentedObjectAtIndexPath indexPath: NSIndexPath) {
+        item.textField?.stringValue = "\(strings[indexPath.item]) \(indexPath.item)"
+    }
+    
+}
