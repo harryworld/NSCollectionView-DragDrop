a9aa3217276bf18d23b01d94c540c67d0cc72164
Harry Ng <harryworld@gmail.com>
2016-03-09 21:23:49 +0800

    Add IBDesignable

--- NSCollectionViewDragDrop/MyView.swift
+++ NSCollectionViewDragDrop/MyView.swift
@@ -8,14 +8,56 @@
 
 import Cocoa
 
-class MyView: NSView {
+@IBDesignable class MyView: NSView {
+    
+    var leftArrowLayer: CAShapeLayer!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        commonInit()
+    }
+    
+    func commonInit() {
+        wantsLayer = true
+        
+        leftArrowLayer = createArrow()
+        leftArrowLayer.opacity = 1
+        layer?.addSublayer(leftArrowLayer)
+    }
 
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
 
         // Drawing code here.
-        NSColor.yellowColor().setFill()
-        NSRectFill(dirtyRect)
+        layer?.backgroundColor = NSColor.yellowColor().CGColor
+        
+        leftArrowLayer.position = NSPoint(x: 30, y: bounds.height / 2)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        layer?.backgroundColor = NSColor.redColor().CGColor
+    }
+    
+    func createArrow() -> CAShapeLayer {
+        let rect = CGRectMake(0, 0, 3, 10)
+        
+        let leftArrow = NSBezierPath()
+        leftArrow.moveToPoint(CGPointMake(0, 0))
+        leftArrow.lineToPoint(CGPointMake(rect.width, rect.height / 2))
+        leftArrow.lineToPoint(CGPointMake(0, rect.height))
+        
+        let shape = CAShapeLayer()
+        shape.strokeColor = NSColor.blueColor().CGColor
+        shape.lineWidth = 2
+        shape.path = leftArrow.toCGPath()
+        shape.bounds = rect
+        
+        return shape
     }
     
 }
