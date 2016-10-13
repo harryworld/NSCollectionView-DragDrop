cd4e26650d1c6853e1cc90be3297582e3cd2a6ce
Harry Ng <harryworld@gmail.com>
2016-10-13 22:01:07 +0800

    Update to Swift 2.3 in Xcode 8

---
+++ NSCollectionViewDragDrop/MyCollectionView.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionView.swift
+//  NSCollectionViewDragDrop
+//
+//  Created by Harry Ng on 13/10/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class MyCollectionView: NSCollectionView {
+
+    override func deselectItemsAtIndexPaths(indexPaths: Set<NSIndexPath>) {
+        super.deselectItemsAtIndexPaths(indexPaths)
+    }
+    
+}
